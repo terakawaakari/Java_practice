f2c2b9be7c3cda6d60e58a3910b3d10de1ec394d
terakawaakari <terakawaakari0701@gmail.com>
2021-08-05 18:25:57 +0900

    繰り返し文追加

--- HelloJava.java
+++ HelloJava.java
@@ -1,5 +1,16 @@
 class HelloJava {
-  public static void main(String args[]){
+  public static void main(String[] args){
+    // 文字列を表示する
     System.out.println("Hello! I'm RecurrentTechnology");
+
+    // 繰り返し文
+    for ( int number = 1; number <=5; number ++) {
+      System.out.println(number);
+    }
+    int sum = 0;
+    for (int number = 1; number <= 10; number++) {
+      sum += number;
+    }
+    System.out.println("sum:" + sum);
   }
 }
